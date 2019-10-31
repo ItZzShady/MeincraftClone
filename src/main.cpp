6aef6100b47565a62847a0f0d1ffdc83391d067c
Richard Lehmann <me@rd-le.com>
2019-10-31 14:57:51 +0100

    Textures can now be loaded

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 int main() {
 
@@ -41,11 +43,15 @@ int main() {
 
         in vec2 position;
         in vec4 color;
+        in vec2 texcoord;
+
         out vec4 color0;
+        out vec2 Texcoord;
 
         void main() {
             gl_Position = vec4(position, 0.0, 1.0); 
             color0 = color;
+            Texcoord = texcoord;
         }
     )glsl";
 
@@ -54,13 +60,16 @@ int main() {
 
         out vec4 outColor;
         in vec4 color0;
+        in vec2 Texcoord;
+
+        uniform sampler2D tex;
 
         void main() {
-            outColor = vec4(color0); 
+            outColor = texture(tex, Texcoord); 
         }
     )glsl";
 
-    enum {POSITION_ATTRIB_LOC, COLOR_ATTRIB_LOC };
+    enum { POSITION_ATTRIB_LOC, COLOR_ATTRIB_LOC, TEXCOORD_ATTRIB_LOC };
 
     glShaderSource(vertexShader, 1, &vertexSource, nullptr);
     glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
@@ -73,6 +82,7 @@ int main() {
 
     glBindAttribLocation(shaderProgram, POSITION_ATTRIB_LOC, "position");
     glBindAttribLocation(shaderProgram, COLOR_ATTRIB_LOC, "color");
+    glBindAttribLocation(shaderProgram, TEXCOORD_ATTRIB_LOC, "texcoord");
 
     glLinkProgram(shaderProgram);
 
@@ -98,6 +108,24 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    const float vertices[] = {
+    //  Position      Color                   TexCoords
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // Top-left
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Top-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f  // Bottom-left
+    };
+
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    float pixels[] = {
+        0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f
+    };
+
     // now on to buffers
     enum {POSITION, COLOR, NUM_BUFFERS };
 
@@ -107,33 +135,42 @@ int main() {
     glBindVertexArray(vao);
 
     glGenBuffers(NUM_BUFFERS, vbo);
-
-    const float vertices[] = {
-    //  Position      Color                   TexCoords
-        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 1.0f, // Top-left
-         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, // Top-right
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 1.0f  // Bottom-left
-    };
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo[POSITION]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &ebo);
-
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    int width, height, channels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* image = stbi_load("sample.png", &width, &height, &channels, STBI_rgb);
+    std::cout <<
+    " width: " << width <<
+    " height: " << height << std::endl;
+    if (image == 0) {
+        std::cout << "Cannot load image." << std::endl;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    stbi_image_free(image);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
     glEnableVertexAttribArray(POSITION_ATTRIB_LOC);
-    glVertexAttribPointer(POSITION_ATTRIB_LOC, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)0);
+    glVertexAttribPointer(POSITION_ATTRIB_LOC, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
 
     glEnableVertexAttribArray(COLOR_ATTRIB_LOC);
-    glVertexAttribPointer(COLOR_ATTRIB_LOC, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 2));
+    glVertexAttribPointer(COLOR_ATTRIB_LOC, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 2));
+
+    glEnableVertexAttribArray(TEXCOORD_ATTRIB_LOC);
+    glVertexAttribPointer(TEXCOORD_ATTRIB_LOC, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
     
     glClearColor(0.1, 0.1, 0.1, 1.0);
     /* Loop unitl the user closes the window */
