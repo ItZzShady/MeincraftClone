6d1be710d1fe17dba9ca67c0b7dba03c44371af8
Richard Lehmann <me@rd-le.com>
2019-10-31 11:33:45 +0100

    Added a quad

--- src/main.cpp
+++ src/main.cpp
@@ -92,13 +92,25 @@ int main() {
     glGenBuffers(NUM_BUFFERS, vbo);
 
     const float vertices[] = {
-         0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
          0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, // Bottom-left
     };
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[POSITION]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(POSITION_ATTRIB_LOC);
     glVertexAttribPointer(POSITION_ATTRIB_LOC, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)0);
@@ -116,7 +128,7 @@ int main() {
         /* Hier Rendern */
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Tausche den front und back Buffer */
         glfwSwapBuffers(window);
